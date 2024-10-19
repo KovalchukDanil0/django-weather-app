6d608e2fd9be6be92fd4c380977d2dff460f47dd
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2024-10-19 22:02:57 +0200

    test

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 echo "BUILD START"
 
 # create a virtual environment named 'venv' if it doesn't already exist
-python -m venv venv
+python3.12 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
