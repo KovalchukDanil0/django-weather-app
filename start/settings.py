93bae65b21ecf738e8eb3cb37866143559ba60af
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2024-10-19 22:01:40 +0200

    test

--- start/settings.py
+++ start/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(environ.get("DEBUG"))
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app", ".now.sh"]
 
