15d26e43a0580609c6fff63f0a2bc88cdd1b4f3a
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2024-10-19 23:30:18 +0200

    test

--- start/asgi.py
+++ start/asgi.py
@@ -3,4 +3,4 @@ from os import environ
 from django.core.asgi import get_asgi_application
 
 environ.setdefault("DJANGO_SETTINGS_MODULE", "start.settings")
-application = get_asgi_application()
+app = get_asgi_application()
