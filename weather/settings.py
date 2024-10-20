c55106a66859121fe8c8d260e07ab6865c18f663
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2024-10-20 16:26:14 +0200

    test

--- weather/settings.py
+++ weather/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = environ.get("DJANGO_DEBUG").lower() == "true"
+DEBUG = environ.get("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app", ".now.sh"]
 
